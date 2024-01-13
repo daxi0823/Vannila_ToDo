36b9d514c7887bfb6956bf1a40e00e32af222d4d
daxi0823 <daxi0823@gmail.com>
2024-01-13 18:41:09 +0900

    create TODO by text

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // import "./style.css";
+
+const onClickAdd = () => {
+  // テキストボックスの内容を受け取って、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  li.appendChild(div);
+
+  document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
